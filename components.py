788d7e5c1c9718c29e5193bd8f50684918975ad2
laragazzadelsole <sara.gironi97@gmail.com>
2023-12-11 17:41:04 +0100

    v2 online

--- components.py
+++ components.py
@@ -230,10 +230,10 @@ def new_app_generation(survey_title,
     base_branch = "main"  # Replace with the branch you want to base the new branch on
 
     # Authenticate with GitHub using your personal access token
-    g = Github(github_token)
+    g = Github("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" )
 
     # Get the repository
-    repo = g.get_repo(repository_name)
+    repo = g.get_repo("laragazzadelsole/Test-Survey")
 
     # Create a new branch
     repo.create_git_ref(ref=f"refs/heads/{branch_name}", sha=repo.get_branch(base_branch).commit.sha)
